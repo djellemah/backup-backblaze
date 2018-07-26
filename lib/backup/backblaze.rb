fb2b6a892ed05e3a71a89d36f6797c9770f05bd2
John Anderson <panic@semiosix.com>
2018-07-26 15:41:26 +0200

    use bundle gem to create skeleton

---
+++ lib/backup/backblaze.rb
@@ -0,0 +1,7 @@
+require "backup/backblaze/version"
+
+module Backup
+  module Backblaze
+    # Your code goes here...
+  end
+end
---
+++ spec/backup/backblaze_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Backup::Backblaze do
+  it "has a version number" do
+    expect(Backup::Backblaze::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
